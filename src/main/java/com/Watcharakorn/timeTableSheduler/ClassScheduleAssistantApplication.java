54d4b72168e67f8085d7917c2b78d7b27e542549
friskkung <6410210283@psu.ac.th>
2023-09-06 19:13:50 +0700

    initiate project

---
+++ src/main/java/com/Watcharakorn/timeTableSheduler/ClassScheduleAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.Watcharakorn.timeTableSheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassScheduleAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassScheduleAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Watcharakorn/timeTableSheduler/ClassScheduleAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Watcharakorn.timeTableSheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassScheduleAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
